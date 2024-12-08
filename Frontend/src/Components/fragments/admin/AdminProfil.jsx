70520f23618776602ee51485111630e04f1f59fa
Rendy <alifrendy663@gmail.com>
2024-12-09 00:17:50 +0700

    fixing error route

--- Frontend/src/Components/fragments/admin/AdminProfil.jsx
+++ Frontend/src/Components/fragments/admin/AdminProfil.jsx
@@ -13,7 +13,7 @@ const AdminProfile = () => {
 
         <div className="flex items-center justify-center">
           <div className="bg-[#222222] w-[1000px] h-[590px] tablets7portrait:w-[1200px] tablets7portrait:h-[1450px] ipadairportrait:w-[1200px] lg:h-[530px] zflip3portrait:h-[700px] iphone14plus:h-[670px] md:h-[1345px] relative rounded-none lg:rounded-2xl md:rounded-none">
-            <div className="absolute -top-8 translate-x-[340px] hidden md:hidden lg:block">
+            <div className="absolute -top-8 translate-x-[440px] hidden md:hidden lg:block">
               <div className="bg-white border border-slate-700 px-32 py-3 rounded-full">
                 <h1 className="text-2xl">Admin Profile</h1>
               </div>
@@ -23,10 +23,10 @@ const AdminProfile = () => {
                 <img
                   src={Bg}
                   alt=""
-                  className="md:w-[800px] lg:w-[430px] md:h-[540px] lg:h-[440px] w-[400px] h-[200px] ipadairportrait:rounded-none zflip3portrait:rounded-xl ipadProPortrait:translate-x-[96px] ipadairportrait:translate-x-[79px] tablets7portrait:translate-x-[78px] md:translate-x-[42px] lg:translate-x-0 rounded-none md:rounded-[30px]"
+                  className="md:w-[800px] lg:rounded-3xl lg:w-[430px] md:h-[540px] lg:h-[440px] w-[400px] h-[200px] ipadairportrait:rounded-none zflip3portrait:rounded-xl ipadProPortrait:translate-x-[96px] ipadairportrait:translate-x-[79px] tablets7portrait:translate-x-[78px] md:translate-x-[42px] lg:translate-x-0 rounded-none md:rounded-[30px]"
                 />
               </div>
-              <div className="bg-white md:w-[800px] lg:w-[630px] md:translate-x-[42px] ipadairportrait:translate-x-[79px] zflip3portrait:mt-6 zflip3portrait:rounded-xl tablets7portrait:h-[720px] ipadProPortrait:translate-x-[96px] tablets7portrait:mt-10 tablets7portrait:translate-x-[78px] ipadairportrait:mt-0 ipadairportrait:rounded-none lg:translate-x-0 md:h-[680px] lg:mt-0 md:mt-7 lg:h-[440px] w-full h-[400px] md:rounded-3xl rounded-none relative">
+              <div className="bg-white md:w-[800px] lg:rounded-3xl lg:w-[630px] md:translate-x-[42px] ipadairportrait:translate-x-[79px] zflip3portrait:mt-6 zflip3portrait:rounded-xl tablets7portrait:h-[720px] ipadProPortrait:translate-x-[96px] tablets7portrait:mt-10 tablets7portrait:translate-x-[78px] ipadairportrait:mt-0 ipadairportrait:rounded-none lg:translate-x-0 md:h-[680px] lg:mt-0 md:mt-7 lg:h-[440px] w-full h-[400px] md:rounded-3xl rounded-none relative">
                 <div className="absolute md:top-10 top-5 flex flex-col iphonexr18:left-16 iphone12:left-14 iphone14plus:left-[78px] left-10 md:gap-y-5 gap-y-2">
                   <div className="flex flex-col">
                     <label htmlFor="" className="font-medium lg:text-lg md:text-[40px]">
@@ -35,7 +35,7 @@ const AdminProfile = () => {
                     <input
                       type="text"
                       readOnly
-                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[400px] lg:h-[38px] w-[200px] h-[30px]"
+                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[450px] lg:h-[38px] w-[200px] h-[30px]"
                     />
                   </div>
                   <div className="flex flex-col">
@@ -45,7 +45,7 @@ const AdminProfile = () => {
                     <input
                       type="text"
                       readOnly
-                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[400px] lg:h-[38px] w-[200px] h-[30px]"
+                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[450px] lg:h-[38px] w-[200px] h-[30px]"
                     />
                   </div>
                   <div className="flex flex-col">
@@ -55,7 +55,7 @@ const AdminProfile = () => {
                     <input
                       type="text"
                       readOnly
-                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[400px] lg:h-[38px] w-[200px] h-[30px]"
+                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[450px] lg:h-[38px] w-[200px] h-[30px]"
                     />
                   </div>
                   <div className="flex flex-col">
@@ -65,11 +65,11 @@ const AdminProfile = () => {
                     <input
                       type="text"
                       readOnly
-                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[400px] lg:h-[38px] w-[200px] h-[30px]"
+                      className="border rounded-3xl border-slate-900 md:w-[650px] md:h-[48px] lg:w-[450px] lg:h-[38px] w-[200px] h-[30px]"
                     />
                   </div>
                 </div>
-                <div className="absolute md:top-[590px] lg:top-[380px] top-[280px] left-7 iphonexr18:left-[53px] iphone12:left-[45px] iphone14plus:left-[66px] md:left-[110px] lg:left-[220px] flex md:w-auto px-4 md:px-0">
+                <div className="absolute md:top-[590px] lg:top-[380px] top-[280px] left-7 iphonexr18:left-[53px] iphone12:left-[45px] iphone14plus:left-[66px] md:left-[110px] lg:left-[270px] flex md:w-auto px-4 md:px-0">
                   <div className="flex flex-col md:flex-row items-center gap-4">
                     <button className="md:py-5 py-2 md:px-28 lg:px-10 lg:py-2 px-[70px] md:text-3xl lg:text-base border border-slate-900 md:rounded-full lg:rounded-3xl rounded-3xl w-full md:w-auto">
                       Edit
