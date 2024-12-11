d819bba3200904459842383bb343595f44407ecc
salsabilatts <salsabilatts03@gmail.com>
2024-12-11 19:21:19 +0700

    admin booking

---
+++ Frontend/src/Pages/admin/booking.jsx
@@ -0,0 +1,14 @@
+import Booking from "../../Components/fragments/admin/Booking";
+import Admin from "../admin";
+
+const BookingPage = () => {
+    return (
+        <>
+            <Admin>
+                <Booking/>
+            </Admin>
+        </>
+    );
+};
+
+export default BookingPage;
--- Frontend/src/routers/index.jsx
+++ Frontend/src/routers/index.jsx
@@ -2,13 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/home";
 import PaymentPages from "../Pages/paymentPages";
 import DetailRoomPages from "../Pages/detailRoom";
-import Booking from "../Pages/booking";
+import Booking from "../Pages/Booking";
 import LoginPage from "../Pages/login";
 import ForgetPasswordPage from "../Pages/forgetPassword";
 import ResetPasswordPage from "../Pages/resetPassword";
 import AdminProfilePage from "../Pages/admin/pofile";
 import RoomPages from "../Pages/admin/room";
 import Dashboard from "../Pages/admin/dasbord";
+import BookingPage from "../Pages/admin/booking";
 
 const router = createBrowserRouter([
   {
@@ -51,6 +52,11 @@ const router = createBrowserRouter([
     path: "/admin/room",
     element: <RoomPages />,
   },
+  {
+    path: "/admin/booking",
+    element: <Booking/>,
+  }
+
 ]);
 
 export default router;
